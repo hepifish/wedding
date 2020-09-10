d4a8e132da2caf6ca6d59de66a306c55ce092071
Patricia Vulc <patricia.vulc@fxbits.io>
2020-09-11 00:22:31 +0300

    sw fix?

--- sw.js
+++ sw.js
@@ -5,12 +5,12 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('weddingTime').then(function(cache) {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/style.css',
-        '/assets/fonts/DroidSerif-Regular.ttf',
-        '/assets/images/leaf-1.png',
-        '/assets/images/leaf-2.png'
+        './',
+        './index.html',
+        './style.css',
+        './assets/fonts/DroidSerif-Regular.ttf',
+        './assets/images/leaf-1.png',
+        './assets/images/leaf-2.png'
       ]);
     })
   );
